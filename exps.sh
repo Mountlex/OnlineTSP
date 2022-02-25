432bbe21c25bfb8b1599db8e25a8b178dd7e26bc
Alexander Lindermayr <linderal@uni-bremen.de>
2022-02-25 16:23:46 +0100

    .

--- exps.sh
+++ exps.sh
@@ -1,5 +1,5 @@
-cargo run --release -- exp1 data/instances --base-sigma=1,5 --num-sigma=20 -o results_s100.csv -s=100
-cargo run --release -- exp1 data/instances --base-sigma=1,5 --num-sigma=20 -o results_s200.csv -s=200
-cargo run --release -- exp1 data/instances --base-sigma=1,5 --num-sigma=20 -o results_s300.csv -s=300
-cargo run --release -- exp1 data/instances --base-sigma=1,5 --num-sigma=20 -o results_s400.csv -s=400
-cargo run --release -- exp1 data/instances --base-sigma=1,5 --num-sigma=20 -o results_s500.csv -s=500
+cargo run --release -- exp1 data/instances --base-sigma=1.5 --num-sigma=20 -o results_s100.csv -s=100
+cargo run --release -- exp1 data/instances --base-sigma=1.5 --num-sigma=20 -o results_s200.csv -s=200
+cargo run --release -- exp1 data/instances --base-sigma=1.5 --num-sigma=20 -o results_s300.csv -s=300
+cargo run --release -- exp1 data/instances --base-sigma=1.5 --num-sigma=20 -o results_s400.csv -s=400
+cargo run --release -- exp1 data/instances --base-sigma=1.5 --num-sigma=20 -o results_s500.csv -s=500
