b910d385171348689a4bbe3849d19e13ce5d093a
Alexander Lindermayr <linderal@uni-bremen.de>
2022-02-28 14:12:24 +0100

    .

--- exps.sh
+++ exps.sh
@@ -1,7 +1,7 @@
 rm results/*
+cargo run --release -- exp1 data/instances_l50 --base-sigma=2 --num-sigma=25 -o results/exp1_s100_l50.csv -s=100
 cargo run --release -- exp1 data/instances_l50 --base-sigma=2 --num-sigma=25 -o results/exp1_s200_l50.csv -s=200
-cargo run --release -- exp1 data/instances_l50 --base-sigma=2 --num-sigma=25 -o results/exp1_s350_l50.csv -s=350
-cargo run --release -- exp1 data/instances_l50 --base-sigma=2 --num-sigma=25 -o results/exp1_s500_l50.csv -s=500
+cargo run --release -- exp1 data/instances_l50 --base-sigma=2 --num-sigma=25 -o results/exp1_s300_l50.csv -s=300
 
 cargo run --release -- exp1 data/instances_l10 --base-sigma=2 --num-sigma=25 -o results/exp1_s100.csv -s=100
 cargo run --release -- exp1 data/instances_l10 --base-sigma=2 --num-sigma=25 -o results/exp1_s150.csv -s=150
--- plot.py
+++ plot.py
@@ -26,11 +26,12 @@ def legend(name, param):
     elif "smart" in name:
         return f"SmartStart"
     elif "pred" in name:
-        return f"SmartTrust (α = {param})"
+        return f"SmartTrust (α={param})"
     else:
         return "unknown"
 
 def plot(filename, save):
+    f = filename.split(".")[0]
     sns.set_theme(style='ticks')
     df = pd.read_csv(filename)
 
@@ -46,28 +47,31 @@ def plot(filename, save):
     ax = sns.lineplot(data=df, x=x_name, y="cr", hue='param', style='param', markers=('frac' in list(df)), linewidth=2.5, markersize=8)
     handlers,_ = ax.get_legend_handles_labels()
  
-    ax.legend(handlers,df['param'].unique(),ncol=2, loc="upper left")
-    
-    if 'sigma' in list(df):
-        ax.set(xscale='symlog')
-        plt.ylim(bottom=0.9, top=4)
-        plt.xlabel("Noise parameter")
-    else:
-        plt.ylim(bottom=0.9, top=4)
-        plt.xlabel("Predicted percentage of actual instance")
-
-    plt.ylabel('Empirical competitive ratio')
-    plt.tight_layout()
-
     fig = plt.gcf()
     fig.set_dpi(600)
     fig.set_size_inches(7,4)
+    plt.ylabel('Empirical competitive ratio')
+    
+    if 'sigma' in list(df):
+        ax.legend(handlers,df['param'].unique(),ncol=2, loc="upper left")
+        ax.set(xscale='symlog')
+        plt.xlabel("Noise parameter")
 
-    if save:
-        f = filename.split(".")[0]
+        plt.ylim(bottom=0.9, top=2.8)
+        plt.tight_layout()
+        plt.savefig(f"{f}_zoom.pdf")
+
+        ax.set(xscale='log')
+        plt.tight_layout()
         plt.savefig(f"{f}.pdf")
     else:
-        plt.show()
+        ax.legend(handlers,df['param'].unique(),ncol=2, loc="upper left")
+        plt.ylim(bottom=0.9, top=2.3)
+        plt.xlabel("Fraction of instance as prediction")      
+        plt.tight_layout()
+        plt.savefig(f"{f}.pdf")
+
+    
 
 
 
