ef27b135e7e81a33c000dbed7ba0c70fc3fee041
Alexander Lindermayr <linderal@uni-bremen.de>
2022-02-28 09:17:31 +0100

    .

--- exps.sh
+++ exps.sh
@@ -1,4 +1,5 @@
-cargo run --release -- exp1 data/instances --base-sigma=1.2 --num-sigma=25 -o results_s100.csv -s=100
-cargo run --release -- exp1 data/instances --base-sigma=1.2 --num-sigma=25 -o results_s200.csv -s=200
-cargo run --release -- exp1 data/instances --base-sigma=1.2 --num-sigma=25 -o results_s300.csv -s=300
-cargo run --release -- exp1 data/instances --base-sigma=1.2 --num-sigma=25 -o results_s400.csv -s=400
+cargo run --release -- exp1 data/instances --base-sigma=1.2 --num-sigma=25 -o results/results_s300.csv -s=300
+cargo run --release -- exp1 data/instances --base-sigma=1.2 --num-sigma=25 -o results/results_s400.csv -s=400
+cargo run --release -- exp1 data/instances --base-sigma=1.2 --num-sigma=25 -o results/results_s400.csv -s=500
+cargo run --release -- exp1 data/instances --base-sigma=1.2 --num-sigma=25 -o results/results_s400.csv -s=600
+cargo run --release -- exp1 data/instances --base-sigma=1.2 --num-sigma=25 -o results/results_s400.csv -s=700
--- taxi.py
+++ taxi.py
@@ -67,7 +67,7 @@ else:
     with open("data/manhattan_zones.json", "r") as file:
         zone_nodes = json.load(file)
 
-m = 5
+m = 50
 for i in range(m):
     generate_instance(f"data/instances/instance{i}.csv",i, m , zone_nodes, length=50)
 
