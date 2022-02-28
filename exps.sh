fb8aa78a92c54d14f400ec9525d8c9c65f7a3944
Alexander Lindermayr <linderal@uni-bremen.de>
2022-02-28 12:00:57 +0100

    .

--- exps.sh
+++ exps.sh
@@ -1,6 +1,11 @@
 rm results/*
-cargo run --release -- exp1 data/instances_l10 --base-sigma=2 --num-sigma=25 -o results/results_s50.csv -s=50
 cargo run --release -- exp1 data/instances_l10 --base-sigma=2 --num-sigma=25 -o results/results_s100.csv -s=100
 cargo run --release -- exp1 data/instances_l10 --base-sigma=2 --num-sigma=25 -o results/results_s150.csv -s=150
+cargo run --release -- exp1 data/instances_l10 --base-sigma=2 --num-sigma=25 -o results/results_s100_wc.csv -s=100 --wc-rd
+cargo run --release -- exp1 data/instances_l10 --base-sigma=2 --num-sigma=25 -o results/results_s150_wc.csv -s=150 --wc-rd
 
+
+cargo run --release -- exp2 data/instances_l10 --pred-frac=0.25 -o results/results_s100_p025.csv -s=100
+cargo run --release -- exp2 data/instances_l10 --pred-frac=0.5 -o results/results_s100_p050.csv -s=100
+cargo run --release -- exp2 data/instances_l10 --pred-frac=0.75 -o results/results_s100_p075.csv -s=100
 
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use anyhow::Result;
 use clap::{Args, Parser};
 use csv::Writer;
-use graphlib::{graphml::graphml_import, sp, tsp, Metric, Node, Nodes, SpMetricGraph};
+use graphlib::{
+    graphml::graphml_import,
+    sp::{self, ShortestPathsCache},
+    tsp, Metric, MetricView, Node, Nodes, SpMetricGraph,
+};
 use oltsp::{
     gaussian_prediction, ignore, instance_from_file, learning_augmented, replan, smartstart,
+    Instance,
 };
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
@@ -13,6 +18,7 @@ use std::path::PathBuf;
 #[derive(Parser)]
 enum Cli {
     Exp1(Exp1),
+    Exp2(Exp2),
 }
 
 #[derive(Args)]
@@ -31,6 +37,30 @@ struct Exp1 {
 
     #[clap(long = "base-sigma", default_value = "2")]
     base_sigma: f64,
+
+    #[clap(short, long, default_value = "1")]
+    scale: usize,
+
+    #[clap(
+        short,
+        long,
+        global = true,
+        default_value = "results.csv",
+        parse(from_os_str)
+    )]
+    output: PathBuf,
+}
+
+#[derive(Args)]
+struct Exp2 {
+    #[clap(parse(from_os_str), value_name = "INSTANCE_DIR")]
+    instance_set: PathBuf,
+
+    #[clap(long, parse(from_os_str), default_value = "data/osm/Manhattan.graphml")]
+    graph: PathBuf,
+
+    #[clap(long = "wc-rd")]
+    wc_rd: bool,
 
     #[clap(long = "pred-frac", default_value = "1.0")]
     rel_num_predictions: f64,
@@ -57,19 +87,43 @@ struct Exp1Result {
     alg: u64,
 }
 
+#[derive(Serialize)]
+struct Exp2Result {
+    name: String,
+    param: f64,
+    frac: f64,
+    opt: u64,
+    alg: u64,
+}
+
+fn compute_wc_instance(instance: Instance, start_node: Node, sp: &ShortestPathsCache) -> Instance {
+    let mut nodes = instance.distinct_nodes();
+    if !nodes.contains(&start_node) {
+        nodes.push(start_node)
+    }
+    let metric_graph = MetricView::from_metric_on_nodes(nodes, sp, None, None);
+    let (_, tour) = tsp::tsp_tour(
+        &metric_graph,
+        start_node,
+        graphlib::tsp::SolutionType::Approx,
+    );
+    let mut reqs: Vec<(Node, usize)> = vec![];
+    let mut t = 0;
+    for edge in tour.windows(2) {
+        t += metric_graph.distance(edge[0], edge[1]).get_usize();
+        reqs.push((edge[1], t));
+    }
+    reqs.into()
+}
+
 fn main() -> Result<()> {
     set_up_logging()?;
     let args = Cli::parse();
 
     match args {
         Cli::Exp1(exp) => {
-            log::info!("Importing graph from file...");
             let graph = graphml_import(exp.graph, None);
-            log::info!("    ...success!");
-
-            log::info!("Importing metric from file...");
             let sp = sp::load_or_compute(&"data/osm/manhattan.dat".into(), &graph)?;
-            log::info!("    ...success!");
 
             let paths: Vec<std::fs::DirEntry> = std::fs::read_dir(exp.instance_set)?
                 .filter_map(|e| e.ok())
@@ -83,33 +137,18 @@ fn main() -> Result<()> {
                     let mut instance = instance_from_file(&file.path()).unwrap();
                     instance.scale_rd(exp.scale);
 
-                    let mut nodes = instance.distinct_nodes();
-                    if !nodes.contains(&start_node) {
-                        nodes.push(start_node)
-                    }
-
                     if exp.wc_rd {
-                        let metric_graph = SpMetricGraph::from_metric_on_nodes(nodes, sp.clone());
-                        let (_, tour) = tsp::tsp_tour(
-                            &metric_graph,
-                            start_node,
-                            graphlib::tsp::SolutionType::Approx,
-                        );
-                        let mut reqs: Vec<(Node, usize)> = vec![];
-                        let mut t = 0;
-                        for edge in tour.windows(2) {
-                            t += metric_graph.distance(edge[0], edge[1]).get_usize();
-                            reqs.push((edge[1], t));
-                        }
-                        instance = reqs.into()
+                        instance = compute_wc_instance(instance, start_node, &sp);
                     }
 
-                    log::info!("Computing lower bound on optimal solution for {:?}...", file.file_name());
-                    let lb = instance.optimal_solution(
-                        start_node,
-                        &sp,
-                        tsp::SolutionType::Approx
-                    ).0.get_usize();
+                    log::info!(
+                        "Computing lower bound on optimal solution for {:?}...",
+                        file.file_name()
+                    );
+                    let lb = instance
+                        .optimal_solution(start_node, &sp, tsp::SolutionType::Approx)
+                        .0
+                        .get_usize();
                     log::info!("    ...success. {}", lb);
 
                     let base_nodes: Vec<Node> = graph.nodes().collect();
@@ -142,13 +181,7 @@ fn main() -> Result<()> {
                         .into_iter()
                         .flat_map(|sigma_num| {
                             let sigma = exp.base_sigma.powi(sigma_num) - 1.0;
-                            let pred = gaussian_prediction(
-                                &instance,
-                                &sp,
-                                &base_nodes,
-                                sigma,
-                                exp.rel_num_predictions,
-                            );
+                            let pred = gaussian_prediction(&instance, &sp, &base_nodes, sigma, 1.0);
                             let mut results: Vec<Exp1Result> = vec![];
 
                             [0.0, 0.1, 0.5, 1.0].iter().for_each(|alpha| {
@@ -199,6 +232,109 @@ fn main() -> Result<()> {
                 .collect();
             export(&exp.output, results)?
         }
+        Cli::Exp2(exp) => {
+            let graph = graphml_import(exp.graph, None);
+            let sp = sp::load_or_compute(&"data/osm/manhattan.dat".into(), &graph)?;
+
+            let paths: Vec<std::fs::DirEntry> = std::fs::read_dir(exp.instance_set)?
+                .filter_map(|e| e.ok())
+                .collect();
+
+            let results: Vec<Exp2Result> = paths
+                .into_par_iter()
+                .flat_map(|file| {
+                    let start_node = 1.into();
+                    log::info!("Loading instance from {:?}", file.path());
+                    let mut instance = instance_from_file(&file.path()).unwrap();
+                    instance.scale_rd(exp.scale);
+                    if exp.wc_rd {
+                        instance = compute_wc_instance(instance, start_node, &sp);
+                    }
+
+                    let lb = instance
+                        .optimal_solution(start_node, &sp, tsp::SolutionType::Approx)
+                        .0
+                        .get_usize();
+
+                    let base_nodes: Vec<Node> = graph.nodes().collect();
+
+                    let t_ignore = ignore(
+                        &graph,
+                        &sp,
+                        instance.clone(),
+                        start_node,
+                        graphlib::tsp::SolutionType::Approx,
+                    ) as u64;
+
+                    let t_replan = replan(
+                        &graph,
+                        &sp,
+                        instance.clone(),
+                        start_node,
+                        graphlib::tsp::SolutionType::Approx,
+                    ) as u64;
+
+                    let t_smart = smartstart(
+                        &graph,
+                        &sp,
+                        instance.clone(),
+                        start_node,
+                        graphlib::tsp::SolutionType::Approx,
+                    ) as u64;
+
+                    let pred = gaussian_prediction(
+                        &instance,
+                        &sp,
+                        &base_nodes,
+                        0.0,
+                        exp.rel_num_predictions,
+                    );
+                    let mut results: Vec<Exp2Result> = vec![];
+
+                    [0.0, 0.1, 0.5, 1.0].iter().for_each(|alpha| {
+                        results.push(Exp2Result {
+                            name: "pred".into(),
+                            param: *alpha,
+                            opt: lb as u64,
+                            alg: learning_augmented(
+                                &graph,
+                                &sp,
+                                instance.clone(),
+                                start_node,
+                                *alpha,
+                                pred.clone(),
+                                graphlib::tsp::SolutionType::Approx,
+                            ) as u64,
+                            frac: exp.rel_num_predictions,
+                        });
+                    });
+
+                    results.push(Exp2Result {
+                        name: "ignore".into(),
+                        param: 0.0,
+                        opt: lb as u64,
+                        alg: t_ignore,
+                        frac: exp.rel_num_predictions,
+                    });
+                    results.push(Exp2Result {
+                        name: "replan".into(),
+                        param: 0.0,
+                        opt: lb as u64,
+                        alg: t_replan,
+                        frac: exp.rel_num_predictions,
+                    });
+                    results.push(Exp2Result {
+                        name: "smart".into(),
+                        param: 0.0,
+                        opt: lb as u64,
+                        alg: t_smart,
+                        frac: exp.rel_num_predictions,
+                    });
+                    results
+                })
+                .collect();
+            export(&exp.output, results)?
+        }
     }
     Ok(())
 }
@@ -222,7 +358,7 @@ fn set_up_logging() -> Result<(), fern::InitError> {
                 message = message
             ));
         })
-        .level(log::LevelFilter::Info)
+        .level(log::LevelFilter::Warn)
         .chain(fern::log_file(format!(
             "logs/{}.log",
             chrono::Local::now().format("%d%m%Y-%H%M")
