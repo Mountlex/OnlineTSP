1acdaace0c21772c8e2a2b42792b548cf394ff53
Alexander Lindermayr <linderal@uni-bremen.de>
2022-02-28 13:46:44 +0100

    .

--- exps.sh
+++ exps.sh
@@ -1,7 +1,6 @@
 rm results/*
-cargo run --release -- exp1 data/instances_l10 --base-sigma=2 --num-sigma=25 -o results/exp1_s100.csv -s=100
-cargo run --release -- exp1 data/instances_l10 --base-sigma=2 --num-sigma=25 -o results/exp1_s150.csv -s=150
+cargo run --release -- exp1 data/instances_l10 --base-sigma=3 --num-sigma=20 -o results/exp1_s100.csv -s=100
+cargo run --release -- exp1 data/instances_l10 --base-sigma=3 --num-sigma=20 -o results/exp1_s150.csv -s=150
 
 cargo run --release -- exp2 data/instances_l10 -o results/exp2_s100_l10.csv -s=100
-cargo run --release -- exp2 data/instances_l50 -o results/exp2_s100_l50.csv -s=100 --num-preds 21
 
