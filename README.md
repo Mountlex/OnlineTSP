7399436a95f07409064464a3f7e6b88a0752c43e
Alexander Lindermayr <linderal@uni-bremen.de>
2022-05-12 09:24:17 +0200

    reproduce

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Implementations of Online TSP and Online Dial-a-Ride Algorithms
+
+## Steps to reproduce results from paper
+
+Install rust: [https://rustup.rs/](https://rustup.rs/)
+
+Run in console
+
+```
+exps.sh
+plots.sh
+```
+
--- exps.sh
+++ exps.sh
@@ -2,8 +2,5 @@ rm results/*
 
 cargo run --release -- exp1 data/instances_l10 --base-sigma=2 --num-sigma=25 -o results/exp1_s100.csv -s=100
 cargo run --release -- exp1 data/instances_l10 --base-sigma=2 --num-sigma=25 -o results/exp1_s100_rd.csv -s=100 --correct-rd
-cargo run --release -- exp1 data/instances_l10 --base-sigma=2 --num-sigma=25 -o results/exp1_s100_loc.csv -s=100 --correct-loc
-
 cargo run --release -- exp2 data/instances_l10 -o results/exp2_s100_l10.csv -s=100
-cargo run --release -- exp2 data/instances_l50 -o results/exp2_s100_l50.csv -s=100 --num-preds=21
 
